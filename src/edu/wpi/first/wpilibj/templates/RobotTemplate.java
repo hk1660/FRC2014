f762322f71e78fc6add383770d99f5fdfb751215
Epsilon Programming <Epsilon Programming@Epsilon>
2014-01-25 17:17:03 -0500

    more drivetrain code

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 
+import com.sun.squawk.util.*;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -22,6 +24,8 @@ import edu.wpi.first.wpilibj.Talon;
  */
 public class RobotTemplate extends IterativeRobot {
 
+    double STARTINGTHRESHOLD = 0.0;
+    
     Talon frontleft;
     Talon frontright;
     Talon backleft;
@@ -58,20 +62,44 @@ public class RobotTemplate extends IterativeRobot {
     
     private void checkDrive(){
         double x = joystick1.getRawAxis(1);
-        if(x < 0.05 && x > -0.05){
+        if(x < 0.1 && x > -0.1){
             x = 0;
         }
+
+        if(x > 0){
+            x = (1-STARTINGTHRESHOLD) * MathUtils.pow(x,2-STARTINGTHRESHOLD) + STARTINGTHRESHOLD;
+        }
+        else if(x < 0){
+            x = -1 * ((1-STARTINGTHRESHOLD) * MathUtils.pow(x,2-STARTINGTHRESHOLD) + STARTINGTHRESHOLD);
+        }
+        
         double y = joystick1.getRawAxis(2);
-        if(y < 0.05 && y > -0.05){
+        if(y < 0.1 && y > -0.1){
             y = 0;
         }
+        if(y > 0){
+            y = (1-STARTINGTHRESHOLD) * MathUtils.pow(y,2-STARTINGTHRESHOLD) + STARTINGTHRESHOLD;
+        }
+        else if(y < 0){
+            y = -1 * ((1-STARTINGTHRESHOLD) * MathUtils.pow(y,2-STARTINGTHRESHOLD) + STARTINGTHRESHOLD);
+        }
+        
         double rotation = joystick1.getRawAxis(3);
         if(rotation < 0.05 && rotation > -0.05){
                 rotation = 0;
         }
+        if(rotation > 0){
+            rotation = (1-STARTINGTHRESHOLD) * MathUtils.pow(rotation,2-STARTINGTHRESHOLD) + STARTINGTHRESHOLD;
+        }
+        else if (rotation < 0){
+            rotation = -1 * ((1-STARTINGTHRESHOLD) * MathUtils.pow(rotation,2-STARTINGTHRESHOLD) + STARTINGTHRESHOLD);
+        }
+        
         double gyroAngle = 0;
         
-        drive.mecanumDrive_Cartesian(x, y, rotation, gyroAngle);
+        System.out.println("x:"+x+" y:"+y+" rotation:"+rotation);
+        
+        drive.mecanumDrive_Cartesian(x, y, rotation, 0.0);
     }
     
     /**
